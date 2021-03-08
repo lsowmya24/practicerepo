b3d5649837a1d0f09b061d50a4c4623b2fa0bcc3
lsowmya <lsowmya@LIN77000910.corp.capgemini.com>
2021-03-08 15:51:47 +0530

    first Commit

---
+++ DevopSimpleDemo/src/main/java/com/cg/boot/MainClass.java
@@ -0,0 +1,19 @@
+package com.cg.boot;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   
+        int[] myarr= {12,45,67,89,23};
+        
+        for(int n:myarr)
+            System.out.println(n);
+        
+        SalesClass class1=new SalesClass();
+        class1.display();
+
+ 
+	}
+
+}
