4515cca30315ec7f6a8e33f1fd0205a28b76e2ab
lsowmya24 <lsowmya@LIN77000910.corp.capgemini.com>
2021-03-09 16:48:12 +0530

    student class added

---
+++ DevopSimpleDemo/src/main/java/com/cg/model/StudentClass.java
@@ -0,0 +1,26 @@
+package com.cg.model;
+
+public class StudentClass {
+
+	private int studentId;
+	private String firstName;
+	private String lastName;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
